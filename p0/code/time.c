d5e8816375eb124cf7c2eb4e9248c6b1506e78b1
Justin Van Dort <jtvd78@aol.com>
2019-01-28 22:32:14 -0500

    Update time.c

--- p0/code/time.c
+++ p0/code/time.c
@@ -1,21 +1,61 @@
+// Little Endian
 
-unsigned int now_usec ( void )
+#include "utils.h"
+
+#define TIMER_CONTROL  0x40000000
+#define TIMER_PRESCALE 0x40000008
+
+#define TIMER_LOW  0x4000001C
+#define TIMER_HIGH 0x40000020 
+
+unsigned long long int now_usec ()
 {
-	// your code goes here
+	return (((unsigned long long int) GET32(TIMER_HIGH)) << 32) + ((unsigned long long int) GET32(TIMER_LOW));
 }
 
-unsigned int usec_diff ( unsigned int, unsigned int )
+unsigned long long int usec_diff ( unsigned long long int now, unsigned long long int before )
 {
-	// your code goes here
+	if (now < before)
+	{
+		return (0xFFFFFFFFFFFFFFFFLL - before) + now;
+	}
+
+	return now - before;
 }
 
 void wait( unsigned int usecs )
 {
-	// your code goes here
+	
+	long long int time_left = usecs;
+	
+	unsigned long long int before = now_usec();
+	while(1)
+	{
+		unsigned long long int now = now_usec();
+		time_left -= usec_diff(now, before);
+
+		if (time_left <= 0) {
+			break;
+		}
+
+		before = now;
+	}
+
+	return;
 }
 
 void init_timer()
 {
-	// your code goes here
-}
+	// Bit 8 -> 0 : 64-bit Core timer runs from the Crystal clock
+	// Bit 9 -> 0 : 64-bit Core timer increments by 1
+	PUT32(TIMER_CONTROL, 0);
 
+	// Sets timer to 1MHz
+	PUT32(TIMER_PRESCALE, 0x06AAAAAB);
+
+	// Reset timer to 0
+	PUT32(TIMER_LOW, 0);
+	PUT32(TIMER_HIGH, 0);
+
+	return;
+}
